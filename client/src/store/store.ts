89e01395408c0fa288a2ec0473e83eb25d73ae82
= <=>
2023-11-17 17:20:33 +0200

    Add Navbar, Router, Wrapper, and AppSlice files
    - Created Navbar.tsx file with a simple component for the navigation
    bar.
    - Created Router.tsx file with a routing configuration using
    react-router-dom.
    - Created Wrapper.tsx file with a wrapper component that includes the
    Navbar and the React Router Outlet.
    - Added AppSlice.ts file with the initial implementation of the app
    state slice.
    - Imported and integrated the AppSlice into the global state in
    store.ts.

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -2,13 +2,15 @@ import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 import { UserState, createUserSlice } from "./UserSlice";
 import { TodoState, createTodoSlice } from './TodoSlice';
+import { AppState, createAppSlice } from './AppSlice';
 
-export const useGlobalState = create<UserState & TodoState>()(
+export const useGlobalState = create<UserState & TodoState & AppState>()(
     devtools(
         persist(
             () => ({
                 ...createUserSlice(),
                 ...createTodoSlice(),
+                ...createAppSlice(),
             }),
             { name: "app-state" }
         )
